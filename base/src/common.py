fb11fe632efae49154150d2fc57db044b53bade0
Santiago Melo Medina <smelomedina05@gmail.com>
2021-10-21 21:37:55 -0500

    deleted signleton temporal class

--- base/src/common.py
+++ base/src/common.py
@@ -1,5 +1,4 @@
 import os
-import pydash
 from pydantic import BaseSettings
 from aws_cdk import core
 
@@ -7,20 +6,6 @@ from aws_cdk import core
 class _Settings(BaseSettings):
     aws_region: str
     aws_account: str
-
-
-class Singletons:
-    def __init__(self):
-        self.__instances = {}
-
-    def __call__(self, _class, *args, **kwargs):
-        class_id = pydash.get(_class, "class_id", default=_class)
-        if class_id not in self.__instances:
-            self.__instances[class_id] = _class(*args, **kwargs)
-        return self.__instances[class_id]
-
-    def get_instances(self):
-        print(self.__instances, sep="\n")
 
 
 _SETTINGS = _Settings()
