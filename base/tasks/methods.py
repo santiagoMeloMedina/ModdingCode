bbc2d1ae774a64ba4bdaa4662789d40cd4280763
Santiago Melo Medina <santiagomelomedina@Santiagos-Mac-mini.local>
2022-01-22 00:56:32 -0500

    addead command clean

--- base/tasks/methods.py
+++ base/tasks/methods.py
@@ -1,12 +1,22 @@
 from subprocess import check_call
+from typing import List
 from dotenv import load_dotenv
 
 
-def hello_there(name: str):
+def __common_call(commands: List[str]):
+    load_dotenv()
+    check_call(args=commands, shell=True)
+
+
+def hello_there(name: str, **kwargs):
     print(f"Hello there {name}!")
 
 
+def clean(**kwargs):
+    command = "black ."
+    __common_call([command])
+
+
 def deploy(**kwargs):
-    load_dotenv()
     command = "cdk deploy"
-    check_call(args=[command], shell=True)
+    __common_call([command])
