6307feb5b566ed79073f641e53acb76e05e1d311
Santiago Melo Medina <santiagomelomedina@Santiagos-Mac-mini.local>
2022-02-13 19:39:32 -0500

    added randomized endpoint to minicourse get

--- logic/src/modding/minicourse/get_minicourse.py
+++ logic/src/modding/minicourse/get_minicourse.py
@@ -12,6 +12,7 @@ class _Settings(settings.Settings):
     thumb_upload_expire_time: str
     multiple_minicourse_retrival_limit: str
     minicourse_username_index_name: str
+    minicourse_category_index_name: str
 
 
 _SETTINGS = _Settings()
@@ -89,12 +90,22 @@ def get_minicourses_by_username(**kwargs) -> Dict[str, Any]:
     return {"minicourses": [minicourse.dict() for minicourse in minicourses]}
 
 
+def get_minicourses_by_category(category_id: str, **kwargs) -> Dict[str, Any]:
+    ### TODO(Santiago): Add randomness and pagination
+    minicourses = MINICOURSE_REPOSITORY.query_items(
+        {"category_id": category_id},
+        index_name=_SETTINGS.minicourse_category_index_name,
+    )
+    return {"minicourses": [minicourse.dict() for minicourse in minicourses]}
+
+
 def actions(action: str, params: Dict[str, Any], **kwargs) -> Dict[str, Any]:
     mapped_actions = {
         "get_minicourse": get_minicourse,
         "get_multiple_minicourses": get_multiple_minicourses,
         "get_minicourse_thumb_upload_url": get_minicourse_thumb_upload_url,
         "get_minicourses_by_username": get_minicourses_by_username,
+        "get_randomized_minicourses": get_minicourses_by_category,
     }
 
     def empty(**kwargs):
