a472612a38f47509ea90515b8426d2c52179bf63
Santiago Melo Medina <smelomedina05@gmail.com>
2022-04-23 19:53:01 -0500

    fixed sending emails from either student or expert

--- logic/src/modding/problem/send_response.py
+++ logic/src/modding/problem/send_response.py
@@ -24,11 +24,13 @@ def handler(event: aws_client.ApiGateway.AGWEvent, context: Dict[str, Any]):
     return response
 
 
-def _send_message_to_student(user_email: str, expert_email: str, message: str) -> None:
+def _send_message_to_student(
+    user_email: str, student_email: str, message: str, **kwargs
+) -> None:
     ses_client = aws_client.ses(user_email)
     ses_client.send_html_email(
-        email_address=expert_email,
+        email_address=student_email,
         subject=EMAIL_SUBJECT.format(username=user_email),
-        html_content=email.generate_message_email_html_template(message),
+        html_content=email.generate_message_email_html_template(user_email, message),
     )
-    _LOGGER.info("Message sent from %s to %s" % (user_email, expert_email))
+    _LOGGER.info("Message sent from %s to %s" % (user_email, student_email))
--- logic/src/modding/utils/email.py
+++ logic/src/modding/utils/email.py
@@ -1,2 +1,8 @@
-def generate_message_email_html_template(message: str) -> str:
-    return ""
+def generate_message_email_html_template(email: str, message: str) -> str:
+    return f"""
+        <h3>User {email} is sending you a message:</h3>
+        <p>{message}</p>
+        <br/>
+        <h5>Start a student, become an expert</h5>
+        <h4>ModdingCode</h4>
+    """
